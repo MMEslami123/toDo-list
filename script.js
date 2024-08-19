9990f487dfa34ca5e71a7bf6949dbaed2b782bde
M.M.Eslami <130648329+MMEslami123@users.noreply.github.com>
2024-08-19 16:44:04 +0330

    click on the trash can and omit a task and create shaking animation for trash can when we hover over it

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const input = document.querySelector("input");
 const ulist = document.querySelector("ul");
 
 button.addEventListener("click", addTask);
-function addTask(event) {
+function addTask() {
   creatItem(input.value);
   input.value = "";
 }
+ulist.addEventListener("click", (event) => {
+  let target = event.target.parentNode.parentNode;
+  if (target.nodeName == "LI") {
+    target.style = "display: none";
+  }
+});
 function creatItem(text) {
   let li = document.createElement("li");
-  console.log(text);
   li.textContent = text;
   li.innerHTML += `<span class="trash-bin"><i class="fa-solid fa-trash-can"></i></span>`;
   ulist.appendChild(li);
