fd336cc2ef97911aa699273e82e3782b8e49de05
M.M.Eslami <130648329+MMEslami123@users.noreply.github.com>
2024-08-29 01:24:10 +0330

    using local storage for save tasks and solve empty task that be created when local storage is empty and optimize some parts

--- script.js
+++ script.js
@@ -1,29 +1,60 @@
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const ulist = document.querySelector("ul");
-
+let myTasks;
+window.addEventListener("load", function () {
+  if (!localStorage.getItem("todo")) {
+    myTasks = [];
+  } else {
+    myTasks = getStorage();
+    updateStorage();
+  }
+});
 button.addEventListener("click", addTask);
 function addTask() {
   if (input.value != "") {
     creatItem(input.value);
+    createStorage(input.value);
     input.value = "";
   } else {
     alert("please add a task...");
   }
 }
 ulist.addEventListener("click", (event) => {
-  let listTarget = event.target;
-  if (listTarget.nodeName == "I") {
-    listTarget.parentNode.parentNode.style = "display: none";
+  if (event.target.nodeName == "I") {
+    let listTarget = event.target.parentNode.parentNode;
+    listTarget.style = "display: none";
+    myTasks.splice(myTasks.indexOf(listTarget.textContent), 1);
+    localStorage.setItem("todo", myTasks);
   }
-  if (listTarget.nodeName == "LI") {
-    listTarget.classList.toggle("active");
+  if (event.target.nodeName == "LI") {
+    event.target.classList.toggle("active");
   }
 });
+
 function creatItem(text) {
   let li = document.createElement("li");
   li.textContent = text;
   li.innerHTML += `<span class="trash-bin"><i class="fa-solid fa-trash-can"></i></span>`;
   ulist.appendChild(li);
   return li;
+}
+
+function createStorage(item) {
+  myTasks.push(item);
+  console.log(myTasks);
+  localStorage.setItem("todo", myTasks);
+}
+
+function getStorage() {
+  return localStorage.getItem("todo").split(",");
+}
+function updateStorage() {
+  for (const item of getStorage()) {
+    if (item == "") {
+      break;
+    } else {
+      creatItem(item);
+    }
+  }
 }
